29492fa5160bbf6b23e32cea720f99d9a536333a
Nicolas Sauzede <nsauzede@laposte.net>
2021-03-05 02:03:01 +0100

    step9: Support multiple statements and 1-letter variables

---
+++ step9/9cc.h
@@ -0,0 +1,73 @@
+#ifndef __9CC_H__
+#define __9CC_H__
+
+#include <stdbool.h>
+
+typedef enum {
+	TK_RESERVED,
+	TK_IDENT,
+	TK_NUM,
+	TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+	TokenKind kind;
+	Token *next;
+	int val;
+	char *str;
+	int len;
+};
+
+typedef enum {
+	ND_EQU,
+	ND_EQUNOT,
+	ND_RELINF,
+	ND_RELINFEQ,
+	ND_RELSUP,
+	ND_RELSUPEQ,
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_ASSIGN,
+	ND_LVAR,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+	int offset;
+};
+
+extern char *user_input;
+extern Token *token;
+extern Node *code[100];
+
+void error_at(char *loc, char *fmt, ...);
+void error(char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize();
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *primary();
+Node *unary();
+Node *mul();
+Node *add();
+Node *relational();
+Node *equality();
+Node *expr();
+void program();
+void gen(Node *node);
+
+#endif/*__9CC_H__*/
