cfb6eb58b646dd0a6a7ac52376307a2a8011cf28
Nicolas Sauzede <nsauzede@laposte.net>
2021-03-10 04:34:18 +0100

    v05: four arith ops

--- V/9cc.c
+++ V/9cc.c
@@ -20,6 +20,51 @@ struct Token {
 };
 
 Token *token;
+char *NodeNames[] = {
+	"ADD",
+	"SUB",
+	"MUL",
+	"DIV",
+	"NUM",
+};
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+typedef struct Node Node;
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+	bool paren;	// false=>none, true=>this node is within '()'
+};
+
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+void info(char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	fprintf(stderr, "\n");
+}
 
 // Function to report an error
 // takes the same arguments as printf
@@ -27,21 +72,7 @@ void error(char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	exit(1);
-}
-
-// Input program
-char *user_input;
-// Report the error location.
-void error_at(char *loc, char *fmt, ...) {
-	va_list ap;
-	va_start(ap, fmt);
-	int pos = loc - user_input;
-	fprintf(stderr, "%s\n", user_input);
-	fprintf(stderr, "%*s", pos, " "); // print pos spaces
-	fprintf(stderr, "^ ");
-	vfprintf(stderr, fmt, ap);
+	va_end(ap);
 	fprintf(stderr, "\n");
 	exit(1);
 }
@@ -99,7 +130,7 @@ Token *tokenize(char *p) {
 			p++;
 			continue;
 		}
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -114,6 +145,88 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+
+Node *primary() {
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		node->paren = true;
+		return node;
+	}
+	return new_node_num(expect_number());
+}
+Node *mul() {
+	Node *node = primary();
+	for (;;) {
+		if (consume('*')) {
+			node = new_node(ND_MUL, node, primary());
+		} else if (consume('/')) {
+			node = new_node(ND_DIV, node, primary());
+		} else {
+			return node;
+		}
+	}
+}
+
+Node *expr() {
+	Node *node = mul();
+	for (;;) {
+		if (consume('+')) {
+			node = new_node(ND_ADD, node, mul());
+		} else if (consume('-')) {
+			node = new_node(ND_SUB, node, mul());
+		} else {
+			return node;
+		}
+	}
+}
+
+void gen(Node *node) {
+	// info("%s", NodeNames[node->kind]);
+	if (node->paren) {
+		printf("(");
+	}
+	if (node->kind == ND_NUM) {
+		printf("%d", node->val);
+		return;
+	}
+	gen(node->lhs);
+	switch (node->kind) {
+		case ND_ADD:
+			printf("+");
+			break;
+		case ND_SUB:
+			printf("-");
+			break;
+		case ND_MUL:
+			printf("*");
+			break;
+		case ND_DIV:
+			printf("/");
+			break;
+	}
+	gen(node->rhs);
+	if (node->paren) {
+		printf(")");
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <number>\n", argv[0]);
@@ -121,14 +234,9 @@ int main(int argc, char *argv[]) {
 	}
 	user_input = argv[1];
 	token = tokenize(user_input);
-	printf("exit(%d", expect_number());
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("+%d", expect_number());
-		}
-		expect('-');
-		printf("-%d", expect_number());
-	}
+	Node *node = expr();
+	printf("exit(");
+	gen(node);
 	printf(")\n");
 	return 0;
 }
