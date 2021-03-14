f5e265c59f6e1e60f96256f0dbc2e168da6466fb
Nicolas Sauzede <nsauzede@laposte.net>
2021-03-15 00:31:56 +0100

    Refactor code on chibicc

--- new/codegen.c
+++ new/codegen.c
@@ -3,178 +3,256 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "9cc.h"
 
-int label_seq = 0;
+static Obj *current_fn;
+static char *argreg[] = {"%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"};
+static int depth;
+
+static void gen_expr(Node *node);
+
+static int count() {
+	static int i = 1;
+	return i++;
+}
+
+static void push(void) {
+  printf("\tpush %%rax\n");
+  depth++;
+}
+  
+static void pop(char *arg) {
+  printf("\tpop %s\n", arg);
+  depth--;
+}
+
+static int align_to(int n, int align) {
+  return (n + align - 1) / align * align;
+}
+
+static void gen_addr(Node *node) {
+  switch (node->kind) {
+  case ND_VAR:
+    if (node->var->is_local) {
+      // Local variable
+      printf("\tlea %d(%%rbp), %%rax\n", node->var->offset);
+    } else {
+      // Global variable
+      printf("\tlea %s(%%rip), %%rax\n", node->var->name);
+    }
+    return;
+  case ND_DEREF:
+    gen_expr(node->lhs);
+    return;
+  }
+
+  error_tok(node->tok, "not an lvalue");
+}
 
 static void gen_lval(Node *node) {
-	if (node->kind != ND_LVAR) {
+	if (node->kind != ND_VAR) {
 		error("Left side value of assignment is not a variable");
 	}
 	printf("\tmov rax, rbp\n");
-	printf("\tsub rax, %d\n", node->lvar->offset * 8);
+	printf("\tsub rax, %d\n", node->var->offset * 8);
 	printf("\tpush rax\n");
 }
 
-static void gen(Node *node) {
-	if (!node) {
-		return;
-	}
+// Load a value from where %rax is pointing to.
+static void load(Type *ty) {
+  if (ty->kind == TY_ARRAY) {
+    // If it is an array, do not attempt to load a value to the
+    // register because in general we can't load an entire array to a
+    // register. As a result, the result of an evaluation of an array
+    // becomes not the array itself but the address of the array.
+    // This is where "array is automatically converted to a pointer to
+    // the first element of the array in C" occurs.
+    return;
+  }
+    
+  printf("\tmov (%%rax), %%rax\n");
+}   
+  
+// Store %rax to an address that the stack top is pointing to.
+static void store(void) {
+  pop("%rdi");
+  printf("\tmov %%rax, (%%rdi)\n");
+}
+
+static void gen_expr(Node *node) {
 	switch (node->kind) {
-		case ND_CALL:
-		{
-			LVar *lvar = (LVar *)node->lhs;
-			char buf[1000];
-			snprintf(buf, lvar->len + 1, "%s", lvar->name);
-			// printf("\tpop \n", buf);
-			Node *next = node->rhs;
-			int nargs = 0;
-			while (next) {
-				gen(next);
-				next = next->next;
-				// fprintf(stderr, "ARG!\n");
-				nargs++;
-			}
-			if (nargs>=2) {
-				printf("\tpop rsi\n");
-			}
-			if (nargs>=1) {
-				printf("\tpop rdi\n");
-			}
-			// Following RSP 16-byte adjustment is important to respect x64 ABI
-			printf("\tand rsp, 0xfffffffffffffff0\n");
-			printf("\tcall %s\n", buf);
-			printf("\tpush rax\n");
-			return;
-		}
-		case ND_RETURN:
-			gen(node->lhs);
-			printf("\tpop rax\n");
-			printf("\tmov rsp,rbp\n");
-			printf("\tpop rbp\n");
-			printf("\tret\n");
-			return;
-		case ND_BLOCK:
-			for (Node *next = node->body; next; next = next->next) {
-				gen(next);
-				printf("\tpop rax\n");
-			}
-			return;
-		case ND_FOR:
-			gen(node->init);
-			printf("\t.Lbegin%d:\n", label_seq);
-			gen(node->cond);
-			printf("\tpop rax\n");
-			printf("\tcmp rax, 0\n");
-			printf("\tje .Lend%d\n", label_seq);
-			gen(node->then);
-			gen(node->inc);
-			printf("\tjmp .Lbegin%d\n", label_seq);
-			printf("\t.Lend%d:\n", label_seq);
-			label_seq++;
-			return;
-		case ND_IF:
-			gen(node->cond);
-			printf("\tpop rax\n");
-			printf("\tcmp rax, 0\n");
-			if (node->else_) {
-				printf("\tje .Lelse%d\n", label_seq);
-				gen(node->then);
-				printf("\tjmp .Lend%d\n", label_seq);
-				printf("\t.Lelse%d:\n", label_seq);
-				gen(node->else_);
-			} else {
-				printf("\tje .Lend%d\n", label_seq);
-				gen(node->then);
-			}
-			printf("\t.Lend%d:\n", label_seq);
-			label_seq++;
-			return;
-		case ND_WHILE:
-			printf("\t.Lwhilebegin%d:\n", label_seq);
-			gen(node->cond);
-			printf("\tpop rax\n");
-			printf("\tcmp rax, 0\n");
-			printf("\tje .Lwhileend%d\n", label_seq);
-			gen(node->then);
-			printf("\tjmp .Lwhilebegin%d\n", label_seq);
-			printf("\t.Lwhileend%d:\n", label_seq);
-			label_seq++;
-			return;
 		case ND_NUM:
-			printf("\tpush %d\n", node->val);
+			printf("\tmov $%d, %%rax\n", node->val);
 			return;
-		case ND_LVAR:
-			gen_lval(node);
-			printf("\tpop rax\n");
-			printf("\tmov rax, [rax]\n");
-			printf("\tpush rax\n");
+		case ND_NEG:
+			gen_expr(node->lhs);
+			printf("\tneg %%rax\n");
+			return;
+		case ND_VAR:
+			gen_addr(node);
+			load(node->ty);
 			return;
 		case ND_ASSIGN:
-			gen_lval(node->lhs);
-			gen(node->rhs);
-			printf("\tpop rdi\n");
-			printf("\tpop rax\n");
-			printf("\tmov [rax], rdi\n");
-			printf("\tpush rdi\n");
+			gen_addr(node->lhs);
+			push();
+			gen_expr(node->rhs);
+			store();
+			return;
+		case ND_FUNCALL: ;
+			int nargs = 0;
+			for (Node *arg = node->args; arg; arg = arg->next) {
+				gen_expr(arg);
+				push();
+				nargs++;
+			}
+			for (int i = nargs - 1; i >= 0; i--) {
+				pop(argreg[i]);
+			}
+			// Following RSP 16-byte adjustment is important to respect x64 ABI
+			// printf("\tand rsp, 0xfffffffffffffff0\n");
+			printf("\tmov $0, %%rax\n");
+			printf("\tcall %s\n", node->funcname);
 			return;
 	}
-	gen(node->lhs);
-	gen(node->rhs);
-	printf("\tpop rdi\n");
-	printf("\tpop rax\n");
+	gen_expr(node->rhs);
+	push();
+	gen_expr(node->lhs);
+	pop("%rdi");
 	switch (node->kind) {
-		case ND_EQU:
-			printf("\tcmp rax, rdi\n");
-			printf("\tsete al\n");
-			printf("\tmovzb rax, al\n");
-			break;
-		case ND_EQUNOT:
-			printf("\tcmp rax, rdi\n");
-			printf("\tsetne al\n");
-			printf("\tmovzb rax, al\n");
-			break;
-		case ND_RELINF:
-			printf("\tcmp rax, rdi\n");
-			printf("\tsetl al\n");
-			printf("\tmovzb rax, al\n");
-			break;
-		case ND_RELINFEQ:
-			printf("\tcmp rax, rdi\n");
-			printf("\tsetle al\n");
-			printf("\tmovzb rax, al\n");
-			break;
-		case ND_RELSUP:
-			printf("\tcmp rdi, rax\n");
-			printf("\tsetl al\n");
-			printf("\tmovzb rax, al\n");
-			break;
-		case ND_RELSUPEQ:
-			printf("\tcmp rdi, rax\n");
-			printf("\tsetle al\n");
-			printf("\tmovzb rax, al\n");
-			break;
 		case ND_ADD:
-			printf("\tadd rax,rdi\n");
-			break;
+			printf("\tadd %%rdi, %%rax\n");
+			return;
 		case ND_SUB:
-			printf("\tsub rax,rdi\n");
-			break;
+			printf("\tsub %%rdi, %%rax\n");
+			return;
 		case ND_MUL:
-			printf("\timul rax,rdi\n");
-			break;
+			printf("\timul %%rdi, %%rax\n");
+			return;
 		case ND_DIV:
 			printf("\tcqo\n");
-			printf("\tidiv rdi\n");
-			break;
-		default:
-			error("%s: Unknown node type %d\n", __func__, node->kind);
+			printf("\tidiv %%rdi\n");
+			return;
+		case ND_EQ:
+		case ND_NE:
+		case ND_LT:
+		case ND_LE:
+			printf("\tcmp %%rdi, %%rax\n");
+			if (node->kind == ND_EQ)
+				printf("\tsete %%al\n");
+			else if (node->kind == ND_NE)
+				printf("\tsetne %%al\n");
+			else if (node->kind == ND_LT)
+				printf("\tsetl %%al\n");
+			else if (node->kind == ND_LE)
+				printf("\tsetle %%al\n");
+			printf("\tmovzb %%al, %%rax\n");
+			return;
 	}
-	printf("\tpush rax\n");
+	error_tok(node->tok, "invalid expression");
 }
 
-void generate() {
+static void gen_stmt(Node *node) {
+	switch (node->kind) {
+		case ND_IF: {
+			int c = count();
+			gen_expr(node->cond);
+			printf("\tcmp $0, %%rax\n");
+			printf("\tje .L.else.%d\n", c);
+			gen_stmt(node->then);
+			printf("\tjmp .L.end.%d\n", c);
+			printf(".L.else.%d:\n", c);
+			if (node->els)
+				gen_stmt(node->els);
+			printf(".L.end.%d:\n", c);
+			return;
+		}
+		case ND_FOR: {
+			int c = count();
+			if (node->init)
+				gen_stmt(node->init);
+			printf(".L.begin.%d:\n", c);
+			if (node->cond) {
+				gen_expr(node->cond);
+				printf("\tcmp $0, %%rax\n");
+				printf("\tje .L.end.%d\n", c);
+			}
+			gen_stmt(node->then);
+			if (node->inc)
+				gen_expr(node->inc);
+			printf("\tjmp .L.begin.%d\n", c);
+			printf(".L.end.%d:\n", c);
+			return;
+		}
+		case ND_BLOCK:
+			for (Node *n = node->body; n; n = n->next)
+				gen_stmt(n);
+			return;
+		case ND_RETURN:
+			gen_expr(node->lhs);
+			printf("\tjmp .L.return.%s\n", current_fn->name);
+			return;
+		case ND_EXPR_STMT:
+			gen_expr(node->lhs);
+			return;
+	}
+	error_tok(node->tok, "invalid statement");
+}
+
+static void assign_lvar_offsets(Obj *prog) {
+  for (Obj *fn = prog; fn; fn = fn->next) {
+    if (!fn->is_function)
+      continue;
+
+    int offset = 0;
+    for (Obj *var = fn->locals; var; var = var->next) {
+      offset += var->ty->size;
+      var->offset = -offset;
+    }
+    fn->stack_size = align_to(offset, 16);
+  }
+}
+
+void emit_data(Obj *prog) {
+	for (Obj *obj = prog; obj; obj = obj->next) {
+		if (obj->is_function) {
+			continue;
+		}
+		continue;
+	}
+}
+
+void emit_text(Obj *prog) {
+	for (Obj *obj = prog; obj; obj = obj->next) {
+		if (!obj->is_function) {
+			continue;
+		}
+		printf(".globl %s\n", obj->name);
+		printf("\n");
+		printf("%s:\n", obj->name);
+		current_fn = obj;
+		// prologue
+		printf("\tpush %%rbp\n");
+		printf("\tmov %%rsp, %%rbp\n");
+		printf("\tsub $%d, %%rsp\n", obj->stack_size);
+		// save passed-by-registers args to the stack
+		int i = 0;
+		for (Obj *var = obj->params; var; var = var->next) {
+			printf("\tmov %s, %d(%%rbp)\n", argreg[i++], var->offset);
+		}
+		// emit code
+		gen_stmt(obj->body);
+		assert(depth == 0);
+		// epilogue
+		printf(".L.return.%s:\n", obj->name);
+		printf("\tmov %%rbp,%%rsp\n");
+		printf("\tpop %%rbp\n");
+		printf("\tret\n");
+	}
+}
+
+void codegen(Obj *prog) {
 	int gen_v = 0;
 	char *env = getenv("GEN_V");
 	if (env) {
@@ -182,16 +260,13 @@ void generate() {
 	}
 	if (gen_v) {
 		extern void generate_v();
-		generate_v();
+		// generate_v();
 		return;
 	}
-	printf(".intel_syntax noprefix\n");
-	printf(".globl main\n");
-	printf("\n");
-	printf("main:\n");
-	printf("\tpush rbp\n");
-	printf("\tmov rbp, rsp\n");
-	printf("\tsub rsp, 208\n");
+	assign_lvar_offsets(prog);
+	emit_data(prog);
+	emit_text(prog);
+/*
 	Node *node = code;
 	while (node) {
 		// printf("//node %d\n", node->kind);
@@ -199,7 +274,5 @@ void generate() {
 		// printf("\tpop rax\n");
 		node = node->next;
 	}
-	printf("\tmov rsp,rbp\n");
-	printf("\tpop rbp\n");
-	printf("\tret\n");
+*/
 }
