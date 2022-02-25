da3638abab4ebe063988948c10a555b055374304
Nicolas Sauzede <nsauzede@laposte.net>
2022-02-25 02:35:51 +0100

    Restore upstream common by fixing ASSERT macro; TODO: implement do{}while

--- step28/test/test.h
+++ step28/test/test.h
@@ -1,4 +1,1 @@
-//#define ASSERT(x, y) assert2(__FILE__, __LINE__, x, y, #y)
-//#include "common"
-
-#define ASSERT(x, y) assert(x, y, #y)
+#define ASSERT(x, y) printf("%s:%d:", __FILE__, __LINE__);assert(x, y, #y)
